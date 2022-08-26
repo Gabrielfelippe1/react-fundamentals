a875ff03db50b3c1220b4a5da1051828e7e67c05
gabrielfelipe <gabrielfelipe.a2000@gmail.com>
2022-08-26 18:13:29 -0300

    fix: formatar projeto

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,34 +1,36 @@
-import styles from './Comment.module.css';
+import styles from './Comment.module.css'
 
-import { ThumbsUp, Trash } from 'phosphor-react';
-import { Avatar } from './Avatar';
-import { useState } from 'react';
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
 interface CommentProps {
-  content: string;
+  content: string
   onDeleteComment: (comment: string) => void
 }
-export function Comment({content, onDeleteComment} : CommentProps) {
+export function Comment ({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0)
-  function handleDeleteComment(){
+  function handleDeleteComment () {
     console.log('deletar')
     onDeleteComment(content)
   }
-  function handleLikeComment(){
-    setLikeCount(likeCount+ 1 )
+  function handleLikeComment () {
+    setLikeCount(likeCount + 1)
   }
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+      <Avatar hasBorder={false} src='https://github.com/diego3g.png' alt='' />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Diego Fernandes</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+              <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:00'>
+                Cerca de 1h atrás
+              </time>
             </div>
 
-            <button onClick={handleDeleteComment} title="Deletar comentário">
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,51 +1,54 @@
-import { Avatar } from './Avatar';
-import { Comment } from './Comment';
-import {format, formatDistanceToNow} from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR';
-import styles from './Post.module.css';
-import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import styles from './Post.module.css'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 
-interface Author{
-  name: string;
-  role: string;
+interface Author {
+  name: string
+  role: string
   avatarUrl: string
 }
 interface Content {
-  type: 'paragraph' | 'link';
+  type: 'paragraph' | 'link'
   content: string
 }
 interface PostProps {
-  author: Author;
-  publishedAt: Date;
+  author: Author
+  publishedAt: Date
   content: Content[]
 }
 
-export function Post({author, publishedAt, content} : PostProps) {
+export function Post ({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState(['Post muito bacana ein'])
   const [newCommentText, setNewCommentText] = useState('')
-  const publishedDateFormated = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR
-  })
+  const publishedDateFormated = format(
+    publishedAt,
+    "dd 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR
+    }
+  )
   const publisehDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
-
   })
-  
-  function handleCreateNewComment(event : FormEvent){
+
+  function handleCreateNewComment (event: FormEvent) {
     event.preventDefault()
     setComments([...comments, newCommentText])
     setNewCommentText('')
   }
-  function handleNewCommentChange(event : ChangeEvent<HTMLTextAreaElement>){
+  function handleNewCommentChange (event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
   }
-  function handleNewCommentInvalid(event : InvalidEvent<HTMLTextAreaElement>){
+  function handleNewCommentInvalid (event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('Esse campo é necessário')
   }
-  function deleteComment(commentToDelete: string){
-    const commentsWithoutDeletedOne = comments.filter(comment =>{
+  function deleteComment (commentToDelete: string) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete
     })
     setComments(commentsWithoutDeletedOne)
@@ -61,15 +64,22 @@ export function Post({author, publishedAt, content} : PostProps) {
             <span>{author.role}</span>
           </div>
         </div>
-        <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>{publisehDateRelativeToNow}</time>
+        <time
+          title={publishedDateFormated}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publisehDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        {content.map( (line) => {
-          if(line.type === 'paragraph'){
+        {content.map(line => {
+          if (line.type === 'paragraph') {
             return <p key={line.content}>{line.content}</p>
-          }else if(line.type === 'link'){
-            <p key={line.content}><a href="#">{line.content}</a></p>
+          } else if (line.type === 'link') {
+            ;<p key={line.content}>
+              <a href='#'>{line.content}</a>
+            </p>
           }
         })}
       </div>
@@ -79,7 +89,7 @@ export function Post({author, publishedAt, content} : PostProps) {
 
         <textarea
           name='comment'
-          placeholder="Deixe um comentário"
+          placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
           onInvalid={handleNewCommentInvalid}
@@ -87,14 +97,22 @@ export function Post({author, publishedAt, content} : PostProps) {
         />
 
         <footer>
-          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-       {comments.map((comment) => {
-        return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
-       })}
+        {comments.map(comment => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
       </div>
     </article>
   )
